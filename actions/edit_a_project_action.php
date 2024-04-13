f33bf42149830c96a19c590cec409e9a9fe06e14
csafopoku <116459270+csafopoku@users.noreply.github.com>
2024-04-13 12:08:41 +0000

    d

--- actions/edit_a_project_action.php
+++ actions/edit_a_project_action.php
@@ -1,7 +1,6 @@
 <?php
 include "../settings/connection.php";
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+
 if(isset($_POST['submit'])){
     $projectName =  $_POST['title'];
     $overview = $_POST['overview'];
@@ -17,7 +16,6 @@ if(isset($_POST['submit'])){
     $result = mysqli_query($conn, $sql);
 
     if($result) {
-
         header("Location: ../admin/project_control_view.php");
     } else {
         echo "Connection failed";
