982178a4181140b55263d81b3b195a41b61eaf67
csafopoku <116459270+csafopoku@users.noreply.github.com>
2024-04-13 12:00:21 +0000

    s

--- actions/edit_a_project_action.php
+++ actions/edit_a_project_action.php
@@ -7,12 +7,12 @@ if(isset($_POST['submit'])){
     $projectID =  $_GET['project_id'];
     $category = $_POST['category']; 
     $difficulty = $_POST['difficulty_level'];
-    $url = $_POST['image_url'];
+    // $url = $_POST['image_url'];
     $description = $_POST['description'];
     $materials = $_POST['materials_needed'];
    
     // $sql = "UPDATE Projects SET title='$projectName' WHERE project_id='$projectID'";
-    $sql = "UPDATE Projects SET title='$projectName', overview='$overview', category='$category', difficulty_level='$difficulty', image_url='$url', description='$description', materials_needed='$materials' WHERE project_id='$projectID'";
+    $sql = "UPDATE Projects SET title='$projectName', overview='$overview', category='$category', difficulty_level='$difficulty', description='$description', materials_needed='$materials' WHERE project_id='$projectID'";
     $result = mysqli_query($conn, $sql);
     var_dump($result);
 
