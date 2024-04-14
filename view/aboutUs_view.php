335710c1796dcea496219e6057d7222802222c53
csafopoku <116459270+csafopoku@users.noreply.github.com>
2024-04-14 16:57:09 +0000

    ff

--- view/aboutUs_view.php
+++ view/aboutUs_view.php
@@ -40,7 +40,7 @@
             <p>Kid Wiz is a magical place where learning and fun intersect, creating unforgettable experiences for kids. Founded by a team of passionate educators and parents, Kid Wiz aims to ignite the spark of curiosity in every child, making education an adventure filled with excitement and discovery.</p>
             <p>Our journey began with a simple question: How can we make learning so engaging that kids always come back for more? This question led us to create Kid Wiz, a platform where kids can explore various subjects through interactive lessons, games, and creative projects.</p>
             <p>From science experiments that amaze to crafts that inspire creativity, Kid Wiz offers a diverse range of activities designed to stimulate young minds. We believe in the power of hands-on learning and the importance of fostering a love for knowledge from an early age.</p>
-            <a href="../view/aboutUs_view.php" class="btn">discover more</a>
+            <a href="../view/landing_view.php" class="btn">Back to Home</a>
         </div>
     </div>
 </section>
